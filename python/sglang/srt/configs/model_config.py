sgl-project/sglang

feat(spec): allow independent draft model config overrides
Target and draft checkpoints can require incompatible Hugging Face config overrides. Previously --json-model-override-args was applied to both checkpoints, including algorithm alias detection, DFlash inference, and DSpARK draft inspection.

Add a draft-specific override with backward-compatible fallback to the target override, and use it consistently for draft configuration loading.

python/sglang/srt/configs/model_config.py
@@ -587,7 +587,11 @@ class ModelConfig:
                 if context_length is not None
                 else server_args.context_length
             ),
-            model_override_args=server_args.json_model_override_args,
+            model_override_args=(
+                server_args.get_draft_model_override_args()
+                if is_draft_model
+                else server_args.json_model_override_args
+            ),
             is_embedding=server_args.is_embedding,
             enable_multimodal=server_args.enable_multimodal,
             dtype=server_args.dtype,

jpezzulli/sglang-rtxpro6000

python/sglang/srt/configs/model_config.py
@@ -609,7 +609,15 @@ class ModelConfig:
             context_length=(
                 context_length if context_length is not None else cfg.context_length
             ),
-            model_override_args=cfg.json_model_override_args,
+            model_override_args=(
+                (
+                    cfg.speculative_draft_model_override_args
+                    if cfg.speculative_draft_model_override_args is not None
+                    else cfg.json_model_override_args
+                )
+                if is_draft_model
+                else cfg.json_model_override_args
+            ),
             is_embedding=cfg.is_embedding,
             enable_multimodal=cfg.enable_multimodal,
             dtype=cfg.dtype,